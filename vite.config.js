98e760e390ab3ab46f09d44f211ae80626207a76
Gyanesh Gouraw <gyanesh.gouraw@okta.com>
2025-06-24 10:14:08 +0530

    chore updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // This sets the base path for the application, useful for deployment in subdirectories
 })
